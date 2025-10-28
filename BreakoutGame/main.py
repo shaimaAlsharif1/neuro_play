247e9d6ea493f39032fba23123b3fca3f3045a86
afnan alhamdan <alhamdanafnan@gmail.com>
2025-10-28 14:53:19 +0300

    added agent file and update main

--- BreakoutGame/main.py
+++ BreakoutGame/main.py
@@ -5,6 +5,7 @@ from PIL import Image
 import torch
 from dqn import DQNBreakout
 from model import AtariNet
+from agent import Agent
 
 os.environ['KMP_duplicate_LIB_OK'] = 'TRUE'
 
@@ -15,13 +16,30 @@ environment = DQNBreakout(device=device,render_mode = 'human')
 
 model = AtariNet(nb_actions=4)
 
+model.to(device)
+
 model.load_model()
 
-state = environment.reset()
+agent = Agent(model= model,
+              device= device,
+              epsilon=1.0,
+              nb_warmup=5000,
+              nb_actions=4,
+              learning_rate=0.00001,
+              memory_capacity=10000,
+              batch_size=64)
 
-print(model.forward(state))
 
-# for _ in range(100):
-#     action = environment.action_space.sample()
-#     state, reward, terminated, truncated , info = environment.step(action)
-    
+agent.train(env=environment, epochs=200000)
+
+# state = environment.reset()
+
+# action_probs = model.forward(state).detach()
+
+# print(f"{action_probs}, {torch.argmax(action_probs, dim=-1, keepdim=True)}")
+
+# print(model.forward(state))
+
+# # for _ in range(100):
+# #     action = environment.action_space.sample()
+# #     state, reward, terminated, truncated , info = environment.step(action)
